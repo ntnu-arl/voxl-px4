7da9067a5c1a922632f0715918faa4aea2e89fea
Eric Katzfey <eric.katzfey@modalai.com>
2022-05-05 10:34:58 -0700

    Added more git diagnostics to build script

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ cd px4-firmware
 echo "*** First describe ***"
 git describe --always --tags --dirty
 echo "*** First log ***"
-git --no-pager log -n 5
-echo "*** pull ***"
-git pull
+git --no-pager log -n 4
+echo "*** Grab all tags ***"
+git fetch --all --tags
 echo "*** Second describe ***"
 git describe --always --tags --dirty
 echo "*** Second log ***"
-git --no-pager log -n 5
+git --no-pager log -n 4
 echo "*** End git diagnostics ***"
 
 make modalai_rb5-flight_default
