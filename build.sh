81acf6b250fd6692e3ed1cd27de8da681131687c
Eric Katzfey <eric.katzfey@modalai.com>
2022-05-05 10:29:35 -0700

    Added git diagnostics to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+echo "*** Starting build ***"
+
 source /home/build-env.sh
 
 cd px4-firmware
+
+echo "*** First describe ***"
+git describe --always --tags --dirty
+echo "*** First log ***"
+git --no-pager log -n 5
+echo "*** pull ***"
+git pull
+echo "*** Second describe ***"
+git describe --always --tags --dirty
+echo "*** Second log ***"
+git --no-pager log -n 5
+echo "*** End git diagnostics ***"
 
 make modalai_rb5-flight_default
 make modalai_rb5-flight_qurt
@@ -10,3 +24,5 @@ make modalai_rb5-flight_qurt
 cat build/modalai_rb5-flight_default/src/lib/version/build_git_version.h
 
 cd -
+
+echo "*** End of build ***"
